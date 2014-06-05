221a0ce6bf066c220f2b997f597bd76a98326b04
Eric Hainer <Eric.Hainer@gmail.com>
2014-06-05 15:50:43 -0600

    Debugging

--- lib/dishwasher/wash.rb
+++ lib/dishwasher/wash.rb
@@ -1,7 +1,5 @@
 module Dishwasher
 	class Wash < ActiveRecord::Base
-		attr_accessible :table, :columns
-
 		def self.ensure_washing(wash, dishes=[])
 			unless find_by_table(wash).count > 0
 				self[:table] = wash
