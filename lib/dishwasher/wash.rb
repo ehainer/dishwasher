3e86513bb3a9513a0fa989748563d6ee0e487595
Eric Hainer <Eric.Hainer@gmail.com>
2014-06-09 16:25:54 -0600

    Added call to destroy any existing wash records before inserting records that would conflict

--- lib/dishwasher/wash.rb
+++ lib/dishwasher/wash.rb
@@ -4,6 +4,7 @@ module Dishwasher
 			dishes = dishes.join(",")
 			existing = where(table: wash, columns: dishes)
 			unless existing.length > 0
+				where(table: wash).destroy_all
 				create(table: wash, columns: dishes)
 			end
 			self
