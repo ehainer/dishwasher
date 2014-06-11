049e3a15a89051dd031f9c46a2f20d0f1add30ea
Eric Hainer <Eric.Hainer@gmail.com>
2014-06-11 14:26:55 -0600

    Debugging, refactored and ran tests

--- lib/dishwasher/load.rb
+++ lib/dishwasher/load.rb
@@ -38,7 +38,7 @@ module Dishwasher
 		def can_select_all?
 			@select_count ||= Dishwasher.chunk_size
 			total_rows = table.all.count
-			return true if @select_count+Dishwasher.state[:offset] > total_rows
+			return true if @select_count+Dishwasher.state[:offset] > total_rows && total_rows > 0
 			false
 		end
 
@@ -55,24 +55,22 @@ module Dishwasher
 		def select_available
 			data = []
 			if can_select_all?
-				puts "SELECT ALL"
 				data = select_all
 			else
-				puts "REMAINDER"
 				data = select_remainder
 			end
 
 			Dishwasher.state[:offset] += data.length
 
+			@select_count = 0 if data.length == 0
+
 			if must_advance?
 				Dishwasher.advance_table
-				puts Dishwasher.state
 			end
 			data
 		end
 
 		def select_all
-			#results = Dishwasher::Load.find_by_sql("SELECT *, '#{Dishwasher.state[:klass]}' AS klass FROM #{table.table_name} OFFSET #{Dishwasher.state[:offset]}")
 			results = table.select(Dishwasher.state[:columns]).offset(Dishwasher.state[:offset])
 			puts "Result Size For " + table.to_s + ": " + results.length.to_s
 			@select_count = @select_count-results.length
@@ -80,7 +78,6 @@ module Dishwasher
 		end
 
 		def select_remainder
-			#results = Dishwasher::Load.find_by_sql("SELECT *, '#{Dishwasher.state[:klass]}' AS klass FROM #{table.table_name} LIMIT #{@select_count} OFFSET #{Dishwasher.state[:offset]}")
 			results = table.select(Dishwasher.state[:columns]).limit(@select_count).offset(Dishwasher.state[:offset])
 			puts "Result Size For " + table.to_s + ": " + results.length.to_s
 			@select_count = @select_count-results.length
