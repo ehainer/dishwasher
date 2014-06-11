e57d62bb6d9dba986c0d10e4a2cf3e18c6a41ea4
Eric Hainer <Eric.Hainer@gmail.com>
2014-06-11 16:21:36 -0600

    Debugging, refactored and ran tests

--- lib/dishwasher/load.rb
+++ lib/dishwasher/load.rb
@@ -81,7 +81,7 @@ module Dishwasher
 
 		def select_remainder
 			results = table.select(Dishwasher.state[:columns]).limit(@select_count).offset(Dishwasher.state[:offset])
-			@select_count = @select_count-@select_count
+			@select_count = @select_count-results.length
 			results
 		end
 
