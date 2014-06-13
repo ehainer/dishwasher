f2ed185cc365432f639f62dade755983f9999075
Eric Hainer <Eric.Hainer@gmail.com>
2014-06-13 09:43:08 -0600

    Changed column from 'table' (reserved) to 'klass' in dishwasher::wash

--- lib/dishwasher/wash.rb
+++ lib/dishwasher/wash.rb
@@ -2,10 +2,10 @@ module Dishwasher
 	class Wash < ActiveRecord::Base
 		def self.ensure_washing(wash, dishes=[])
 			dishes = dishes.join(",")
-			existing = where(table: wash.to_s, columns: dishes)
+			existing = where(klass: wash.to_s, columns: dishes)
 			unless existing.length > 0
-				where(table: wash.to_s).destroy_all
-				create(table: wash.to_s, columns: dishes)
+				where(klass: wash.to_s).destroy_all
+				create(klass: wash.to_s, columns: dishes)
 			end
 		end
 	end
