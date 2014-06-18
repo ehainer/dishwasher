26ba2c92328d6750289b7237525b01cfe7ce87f7
Eric Hainer <Eric.Hainer@gmail.com>
2014-06-18 16:58:54 -0600

    Changed gem version, redirect error message limited to 250 characters

--- lib/dishwasher/load.rb
+++ lib/dishwasher/load.rb
@@ -83,7 +83,7 @@ module Dishwasher
 		end
 
 		def fetch(uri_str, limit = 10, redirects = [])
-			raise Dishwasher::Suds.new("Redirect limit reached. #{redirects.join(", ")}") if limit == 0
+			raise Dishwasher::Suds.new("#{redirects.join(", ")}"[0..250]) if limit == 0
 
 			ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36"
 
